04d09b9c1911a499b468e2a0c0d68101070fc66b
tonlirise <tonlirise@gmail.com>
2022-03-24 23:40:36 +0700

    Добавлен запрос содержимого Контактов

--- app/src/main/java/com/example/getmycontacts/MainActivity.kt
+++ app/src/main/java/com/example/getmycontacts/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.getmycontacts
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.util.Log
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import android.provider.ContactsContract.CommonDataKinds.Phone
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +17,7 @@ class MainActivity : AppCompatActivity() {
 
         if (getCurrentStateContactsPermission()) {
             Log.d(ACTIVITY_LOG_TAG, "access granded")
+            getUserContacts()
         } else {
             Log.d(ACTIVITY_LOG_TAG, "access request")
             requestContactsPermission()
@@ -33,6 +37,33 @@ class MainActivity : AppCompatActivity() {
             READ_CONTACT_RC
         )
 
+    fun getUserContacts() {
+        val cursor = contentResolver.query(
+            ContactsContract.Contacts.CONTENT_URI,
+            null,
+            null,
+            null,
+            null
+        )
+        while (cursor?.moveToNext() == true) {
+            val contactId =
+                cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.Contacts._ID))
+            val contactName =
+                cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.Contacts.DISPLAY_NAME))
+
+            val listNumbers = mutableListOf<String>()
+            val phones = contentResolver.query(
+                Phone.CONTENT_URI, null,
+                Phone.CONTACT_ID + " = " + contactId, null, null
+            )
+            while (phones?.moveToNext() == true) {
+                val number = phones.getString (phones.getColumnIndexOrThrow(Phone.NUMBER));
+                listNumbers.add(number)
+            }
+            Log.d(ACTIVITY_LOG_TAG,"id: $contactId, name: $contactName, phones: $listNumbers")
+        }
+    }
+
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
@@ -42,6 +73,7 @@ class MainActivity : AppCompatActivity() {
         if (requestCode == READ_CONTACT_RC && grantResults.isNotEmpty()) {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Log.d(ACTIVITY_LOG_TAG, "access granded")
+                getUserContacts()
             } else {
                 Log.d(ACTIVITY_LOG_TAG, "access denied")
                 Toast.makeText(this, "Нет доступа к контактам", Toast.LENGTH_LONG).show()
@@ -50,7 +82,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
-        const val ACTIVITY_LOG_TAG = ".MainActivity"
+        const val ACTIVITY_LOG_TAG = ".MainActivity_tag"
         const val READ_CONTACT_RC = 200
     }
 }
